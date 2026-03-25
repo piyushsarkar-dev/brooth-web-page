5d812392c80d1c64fa8493f1332aa1397b1ea00d
Mr Piyush <hi.mrpiyush@gmail.com>
2026-03-26 01:17:18 +0530

    Update HeroSection.tsx

--- src/components/customui/HeroSection.tsx
+++ src/components/customui/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <section className="h-[90vh]">
       <div className="grid h-full place-items-center">
         <BlurText
-          text="Welcome to BROOTH X — the ultimate community "
+          text="Welcome to BROOTH X — the ultimate community."
           delay={200}
           animateBy="words"
           direction="top"
